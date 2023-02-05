ce64c343e777e459a865bb744f77626de4f929f3
Iván <ivantbd87@gmail.com>
2023-02-05 17:42:26 +0100

    app started, theme file created

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Constants from "expo-constants"
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import theme from "./src/utils/theme"
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.safeArea}>
+        <Text>Formulario</Text>
+      </SafeAreaView>
+
+      <View>
+        <Text>Resultado</Text>
+      </View>
+
+      <View>
+        <Text>Footer</Text>
+      </View>
+    </>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  safeArea: {
+    backgroundColor: theme.colors.primary,
+    height: 200, 
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
+    alignItems: "center", 
+    marginTop: Constants.statusBarHeight
+  }
+})
